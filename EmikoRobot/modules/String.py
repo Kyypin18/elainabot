be1ce4345cf5f20b6c614ef320eb3c4e3db53b61
Kyypin18 <101090121+Kyypin18@users.noreply.github.com>
2022-04-14 10:32:06 +0700

    Update String.py

--- EmikoRobot/modules/String.py
+++ EmikoRobot/modules/String.py
@@ -1,3 +1,10 @@
 _mod_name_ = "string"
 
 __help__ = """
+ × /start - 𝙈𝙚𝙢𝙪𝙡𝙖𝙞 𝘽𝙤𝙩
+ × /about - 𝙏𝙚𝙣𝙩𝙖𝙣𝙜 𝘽𝙤𝙩
+ × /ping - Untuk Mengecek Ping Bot
+ × /id - Untuk Mendapatkan User ID
+ × /generate - Mulai Pengambilan String
+ × /cancel - Membatalkan Proses Pengambilan String
+ × /restart - Merestart Proses Pengambilan String
