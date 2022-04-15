1dda62f75d631690b7b5ea68f17bc42b3d648c85
Kyypin18 <101090121+Kyypin18@users.noreply.github.com>
2022-04-15 17:11:07 +0700

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -230,7 +230,6 @@ def start(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
-                disable_web_page_preview=True, # Gitu loh asu Durov kntl
             )
     else:
         update.effective_message.reply_text(
